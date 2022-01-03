f7a94dd1ef24ae5039719b491700e46ea9b366be
Albert Sáez <albertsigmaeng@gmail.com>
2022-01-03 20:57:39 +0100

    95.4%ç

--- classifierlib/train.py
+++ classifierlib/train.py
@@ -3,12 +3,20 @@
 import numpy as np
 import pandas as pd
 import utils
+from numba import jit, cuda
 from datasets import get_train_dataset_directory
 from models import Image, Thresholds
 
 # neural network
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import BaggingClassifier
+
+@jit
+def fit_and_predict(model, X, y, X_test):
+    model.fit(X,y)
+    predict = model.predict(X_test)
+    return model, predict
 
 resistors = pd.read_csv(get_train_dataset_directory() + '/concat_BABITNYPLF.csv')
 resistors['label'] = resistors['label'].astype('category')
@@ -23,13 +31,20 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 print(f'trained Xtrain {X_train.shape} Xtest {X_test.shape} ytrain {y_train.shape} ytest {y_test.shape}')
 
 
-neural_net = MLPClassifier(max_iter=1000000, random_state=1)
-neural_net.fit(X_train, y_train)
+neural_net = MLPClassifier(solver='lbfgs', max_iter=1000000, hidden_layer_sizes=(200,),activation='relu',random_state=1)
+bag = BaggingClassifier(
+    base_estimator=neural_net,
+    n_estimators=10,
+    max_samples=1.0,
+    max_features=1.0,
+    bootstrap=False,
+    n_jobs=1,
+    random_state=1)
+
+bag, y_pred = fit_and_predict(bag, X_train, y_train, X_test)
 
 test = []
 predictions = []
-y_pred = neural_net.predict(X_test)
-
 for predict in y_pred:
 	predictions.append(predict)
 
@@ -40,4 +55,4 @@ for i in range(len(test)):
 	print(f'Got: {predictions[i]}; Expected: {test[i]}; Deviation: {test[i] - predictions[i]}')
 
 print('Misclassified samples: %d' % (y_test != y_pred).sum())
-print('Accuracy: %.2f%%' % (100.0 * neural_net.score(X_test, y_test)))
+print('Accuracy: %.2f%%' % (100.0 * bag.score(X_test, y_test)))
